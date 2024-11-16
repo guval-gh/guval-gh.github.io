6a4595963d4404d74219dc997eea8f8d26a1c2d5
guval <177042063+guval-gh@users.noreply.github.com>
2024-11-16 01:06:09 +0100

    🔧 Reactivate HotReload for web

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StatusBar } from 'expo-status-bar'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   )
@@ -14,8 +14,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  text: {
+    color: 'white'
   }
 })
